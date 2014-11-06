28d081ae61910cb25d3eb6026ea7e89dc2e0e12b
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-11-06 14:32:11 -0300

    Fix metadata error

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Christian A. Rodriguez'
 maintainer_email 'car@cespi.unlp.edu.ar'
 license          'All rights reserved'
 description      'Captures ipaddress from proxmox container'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md')
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
 depends 'ohai',            '~> 2.0'
